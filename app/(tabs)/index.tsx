940d4b5b11a6c7d711824503f2bde71c8b2135d2
bunleaps <bunleap.sorn@gmail.com>
2026-03-04 22:20:29 +0700

    battery status and level working

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,27 @@
-import { useBatteryLevel } from "expo-battery";
+import { BatteryState, useBatteryLevel, useBatteryState } from "expo-battery";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   const batteryLevel = useBatteryLevel();
+  const batteryState = useBatteryState();
+
+  const getStateLabel = (state: BatteryState) => {
+    switch (state) {
+      case BatteryState.CHARGING:
+        return "Charging";
+      case BatteryState.FULL:
+        return "Fully Charged";
+      case BatteryState.UNPLUGGED:
+        return "Unplugged";
+      default:
+        return "Unknown";
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Current Battery Level: {batteryLevel}</Text>
+      <Text style={styles.label}>Battery Level: {batteryLevel}</Text>
+      <Text style={styles.label}>Status: {getStateLabel(batteryState)}</Text>
     </View>
   );
 }
@@ -17,5 +32,10 @@ const styles = StyleSheet.create({
     marginTop: 15,
     alignItems: "center",
     justifyContent: "center",
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginVertical: 5,
   },
 });
